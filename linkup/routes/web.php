0f18e1bf590edd4b87fd065f78162baed0b21cd6
SALWA507 <salwaamzane30@gmail.com>
2026-06-29 17:18:54 +0100

    preparer les root de login et register

--- linkup/routes/web.php
+++ linkup/routes/web.php
@@ -1,5 +1,5 @@
 <?php
-
+use App\Http\Controllers\AuthController;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\PostController;
 
@@ -9,3 +9,5 @@ Route::get('/', function () {
 
 Route::get('/feed', [PostController::class, 'index'])
     ->name('feed');
+Route::get('/register', [AuthController::class, 'showRegister'])->name('show.register');
+Route::get('/login', [AuthController::class, 'showLogin'])->name('show.Login')
