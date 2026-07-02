a471763169db42dd5490aad138bed362732526fe
SALWA507 <salwaamzane30@gmail.com>
2026-07-02 10:17:10 +0100

    faire des modification

--- linkup/routes/web.php
+++ linkup/routes/web.php
@@ -1,20 +1,29 @@
 <?php
 
+use App\Http\Controllers\PostController;
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    return redirect('/feed');
 });
 
-Route::get('/dashboard', function () {
-    return view('dashboard');
-})->middleware(['auth', 'verified'])->name('dashboard');
-
 Route::middleware('auth')->group(function () {
+
+    Route::get('/feed', [PostController::class, 'index'])
+        ->name('feed');
+
+    Route::get('/posts/create', [PostController::class, 'create'])
+        ->name('posts.create');
+
+    Route::get('/dashboard', function () {
+        return redirect()->route('feed');
+    })->name('dashboard');
+
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+
 });
 
 require __DIR__.'/auth.php';
